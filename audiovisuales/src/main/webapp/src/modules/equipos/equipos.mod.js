edb2547f943c82bf7945c6275e0cd25f1e0447e7
German <gc.andrade10@uniandes.edu.co>
2016-08-25 12:21:47 -0500

    actualizado equipos.mod.js

--- audiovisuales/src/main/webapp/src/modules/equipos/equipos.mod.js
+++ audiovisuales/src/main/webapp/src/modules/equipos/equipos.mod.js
@@ -1,1 +1,4 @@
 var mod = ng.module("equiposModule", ["ngMessages"]);
+mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider){
+        $stateProvider.state('estado',{url:"/estado", templateUrl: basePath+"equipos.html"})
+}]);
