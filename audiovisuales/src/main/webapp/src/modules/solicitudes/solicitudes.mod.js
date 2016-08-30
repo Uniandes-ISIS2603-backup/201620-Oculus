dd4b94e203856e9a272536c55ce8a1a993eda2c5
fa.lopez10 <fa.lopez10@uniandes.edu.co>
2016-08-29 20:11:41 -0500

    mod de solicitud

--- audiovisuales/src/main/webapp/src/modules/solicitudes/solicitudes.mod.js
+++ audiovisuales/src/main/webapp/src/modules/solicitudes/solicitudes.mod.js
@@ -1,1 +1,11 @@
-var mod = ng.module("solicitudesModule", ["ngMessages"]);
+var mod = angular.module('solicitudesModule', ['ui.router']);
+    mod.constant("citiesContext", "api/cities");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/solicitudes';
+            $urlRouterProvider.otherwise("/solicitudes");
+            $stateProvider
+                .state('solicitudes'),{
+                    url: "/solicitudes",
+                    templateUrl: basePath + "solicitudes.html"
+                }
+    }]);
