51f8e91ab58e8e1d965054243210b96951e575a0
sneiderV <e.velandia2164@uniandes.edu.co>
2016-08-30 00:18:58 -0500

    modulo Adminitradores (+ CRUD)

--- audiovisuales/src/main/webapp/src/modules/administradores/administradores.mod.js
+++ audiovisuales/src/main/webapp/src/modules/administradores/administradores.mod.js
@@ -1,1 +1,60 @@
-var mod = ng.module("administradoresModule", ["ngMessages"]);
+(function(ng){
+    var mod = ng.module("administradoresModule", ['ui.router']);
+    mod.constant("Contexto de administradores","api/administradores");
+    mode.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+            var direccionInicial='src/modules/administradores';
+            $urlRouterProvide.otherwise("/listaAdministradores");
+            
+  $stateProvider.state('administradoresGet',{
+                url:'/administradores',
+                views: {
+                    'mainView':{
+                        controller: 'administradoresCtrl',
+                        controllerAd: 'ctrl',
+                        templateUrl: direccionInicial + 'administradores.get.html'//Falta crear 
+                    }
+                   }
+          }).state('administradorGet',{
+                url: "/administradores/:administradorId",
+                param: {administradorId: null},
+                views: {
+                    'mainView': {
+                        controller: 'administradoresCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: direccionInicial + 'administradores.getAdministrador.html'
+                        }
+                       }
+          }).state('administradorCreate', {
+                url: '/administradores/create',
+                views: {
+                    'mainView': {
+                        controller: 'administradoresCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: direccionInicial + 'administradores.create.html'
+                        }
+                       }
+          }).state('administradorUpdate', {
+                url: '/administradores/update/:administradordId',
+                param: {administradorId: null},
+                views: {
+                    'mainView': {
+                        controller: 'administradoresCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: direccionInicial + 'administradores.update.html' 
+                    }
+                   }
+          }).state('administradorDelete', {
+                url: '/administradores/delete/:administradorId',
+                param: {administradorId: null},
+                views: {
+                    'mainView': {
+                        controller: 'administradoresCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: direccionInicial + 'administradores.delete.html' 
+                    }
+                   }
+            });  
+        }
+    ]);
+});
+(window.angular);
