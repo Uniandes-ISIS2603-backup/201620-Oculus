fcd23578c278a8f1a18cc34241eb1a6d6e8b9b1b
German <gc.andrade10@uniandes.edu.co>
2016-08-30 12:05:02 -0500

    Index

--- audiovisuales/src/main/webapp/src/app.js
+++ audiovisuales/src/main/webapp/src/app.js
@@ -1,1 +1,16 @@
-var app = ng.module("audiovisuales",["ui-router","solicitudesModule","locacionesModule","equiposModule"]);
+
+
+(function (ng) {
+
+   var mod = ng.module("audiovisuales",["ui-router","solicitudesModule","locacionesModule","equiposModule"]);
+
+    mod.config(['$logProvider', function ($logProvider) {
+            $logProvider.debugEnabled(true);
+        }]);
+
+    mod.config(['$urlRouterProvider', function ($urlRouterProvider) {
+            $urlRouterProvider.otherwise('/citiesList');
+        }]);
+
+  
+})(window.angular);
