edcdafc369ff3acddf8453ba4e3bf70cf5f3369a
German <gc.andrade10@uniandes.edu.co>
2016-08-31 11:36:51 -0500

    Index actualizado se agregaron los modules al app.js

--- audiovisuales/src/main/webapp/src/app.js
+++ audiovisuales/src/main/webapp/src/app.js
@@ -1,7 +1,7 @@
 (function (ng) {
 
    var mod = ng.module("audiovisuales",
-   ["ui.router","ngMessages","equiposModule"]);
+   ["ui.router","ngMessages","equiposModule","administradoresModule","ProfesoresModule,","ModuloLocaciones","solicitudesModule"]);
 
     mod.config(['$logProvider', function ($logProvider) {
             $logProvider.debugEnabled(true);
