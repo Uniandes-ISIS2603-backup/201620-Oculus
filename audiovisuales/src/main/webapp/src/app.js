117359ea5011186dc55b936e522e7ce6f930dd22
fa.lopez10 <fa.lopez10@uniandes.edu.co>
2016-09-21 23:13:10 -0500

    avances date picker

--- audiovisuales/src/main/webapp/src/app.js
+++ audiovisuales/src/main/webapp/src/app.js
@@ -1,22 +1,33 @@
 (function (ng) {
-
-   var mod = ng.module("audiovisuales",
-   ["ui.router","ngMessages",
-       "equiposModule",
-       "puntosDeAtencionModule",
-       "reservasModule",
-       "administradoresModule",
-       "profesoresModule",
-       "administradorGeneralModule"
-   ]);
-
+    
+    var mod = ng.module("audiovisuales",
+    ["ui.router","ngMessages",
+        "equiposModule",
+        "puntosDeAtencionModule",
+        "reservasModule",
+        "administradoresModule",
+        "profesoresModule",
+        "administradorGeneralModule",
+        "datePickerModule"
+    ]);
+    mod.directive('datePicker', [function () {
+            return {
+                scope: {
+                    model: '=',
+                    name: '@'
+                },
+                restrict: 'E',
+                templateUrl: 'src/utils/datepicker.tpl.html',
+                controller: 'datePickerCtrl'
+            };
+        }]);
     mod.config(['$logProvider', function ($logProvider) {
             $logProvider.debugEnabled(true);
         }]);
-
+    
     mod.config(['$urlRouterProvider', function ($urlRouterProvider) {
             $urlRouterProvider.otherwise('/default');
         }]);
-
-  
+    
+    
 })(window.angular);
