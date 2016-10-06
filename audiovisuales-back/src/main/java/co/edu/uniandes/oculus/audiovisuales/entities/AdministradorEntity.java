3204b5cf0f46f638bf39101a5ebcd55c5486f351
sneiderV <e.velandia2164@uniandes.edu.co>
2016-10-06 11:47:10 -0500

    administradorEntity

---
+++ audiovisuales-back/src/main/java/co/edu/uniandes/oculus/audiovisuales/entities/AdministradorEntity.java
@@ -0,0 +1,13 @@
+package co.edu.uniandes.oculus.audiovisuales.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ * @author Sneider Velandia G
+ */
+@Entity
+public class AdministradorEntity extends BaseEntity implements Serializable
+{
+    
+}
