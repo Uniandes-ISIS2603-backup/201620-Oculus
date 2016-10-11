16f7e5732b41ebe7628d5dec6d21ce2769da0cf3
sneiderV <e.velandia2164@uniandes.edu.co>
2016-10-10 22:13:37 -0500

    completo clase AdministradorEntity

--- audiovisuales-back/src/main/java/co/edu/uniandes/oculus/audiovisuales/entities/AdministradorEntity.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/oculus/audiovisuales/entities/AdministradorEntity.java
@@ -2,6 +2,7 @@ package co.edu.uniandes.oculus.audiovisuales.entities;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 /**
  * @author Sneider Velandia G
@@ -9,5 +10,25 @@ import javax.persistence.Entity;
 @Entity
 public class AdministradorEntity extends BaseEntity implements Serializable
 {
+    //atributo que representa el punto de atencion que pertenece un administrador
+    @OneToOne
+    private PuntoDeAtencionEntity puntoDeAtencion;
     
+    /**
+     * dar el punto de atencion al que pertenece un administrador
+     * @return punto de atencion al que pertenece el administrador 
+     */
+    public PuntoDeAtencionEntity getPuntoDeAtencion()
+    {
+        return puntoDeAtencion;
+    }
+    
+    /**
+     * asiganar un punto de atencion a un administrador
+     * @param pda punto de atencion a asignar
+     */
+    public void setPuntoDeAtencion(PuntoDeAtencionEntity pda)
+    {
+        puntoDeAtencion=pda;
+    }
 }
