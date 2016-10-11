30d95b16250bca49a551170497317c53a4892379
sneiderV <e.velandia2164@uniandes.edu.co>
2016-10-10 23:20:21 -0500

    Vol.1 de clase AdministradorTest

--- audiovisuales-back/src/test/java/co/edu/uniandes/oculus/audiovisuales/test/persistence/AdministradorPersistenceTest.java
+++ audiovisuales-back/src/test/java/co/edu/uniandes/oculus/audiovisuales/test/persistence/AdministradorPersistenceTest.java
@@ -5,11 +5,93 @@
  */
 package co.edu.uniandes.oculus.audiovisuales.test.persistence;
 
+import co.edu.uniandes.oculus.audiovisuales.entities.AdministradorEntity;
+import co.edu.uniandes.oculus.audiovisuales.persistence.AdministradorPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /**
  *
  * @author Sneider Velandia G
  */
+@RunWith(Arquillian.class)
 public class AdministradorPersistenceTest
 {
+    /**
+     *
+     * @return el jar que va a desplegar para la prueba
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(AdministradorEntity.class.getPackage())
+                .addPackage(AdministradorPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
     
+    @Inject
+    private AdministradorPersistence administradorPersistence;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    UserTransaction utx;
+
+    private List<AdministradorEntity> data = new ArrayList<AdministradorEntity>();
+    
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void setUp() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from AdministradorEntity").executeUpdate();
+        em.createQuery("delete from PuntoDeAtencionEntity").executeUpdate();
+    }
+    
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            AdministradorEntity entity = factory.manufacturePojo(AdministradorEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
 }
