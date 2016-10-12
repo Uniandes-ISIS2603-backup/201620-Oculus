61a21a150318f6198ee46187abf10d30c592400e
Ana María <am.espinosa11@uniandes.edu.co>
2016-10-11 20:16:01 -0500

    Commit persistence2

---
+++ audiovisuales-back/src/main/java/co/edu/uniandes/oculus/audiovisuales/persistence/PuntoDeAtencionPersistence.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.oculus.audiovisuales.persistence;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import co.edu.uniandes.oculus.audiovisuales.entities.PuntoDeAtencionEntity;
+import java.util.List;
+import javax.persistence.Query;
+/**
+ *
+ * @author am.espinosa11
+ */
+@Stateless
+public class PuntoDeAtencionPersistence 
+{
+     private static final Logger LOGGER = Logger.getLogger(PuntoDeAtencionPersistence.class.getName());
+
+    @PersistenceContext(unitName = "OculusPU")
+    protected EntityManager em;
+    
+     public PuntoDeAtencionEntity find(Long id) 
+     {
+        LOGGER.log(Level.INFO, "Consultando el punto de atención con id={0}", id);
+        return em.find(PuntoDeAtencionEntity.class, id);
+    }
+     
+     public List<PuntoDeAtencionEntity> findAll() {
+        LOGGER.info("Consultando todos los companys");
+        Query q = em.createQuery("select u from CompanyEntity u");
+        return q.getResultList();
+    }
+
+    public PuntoDeAtencionEntity create(PuntoDeAtencionEntity pPunto) 
+    {
+        LOGGER.info("Creando un company nuevo");
+        em.persist(pPunto);
+        LOGGER.info("Company creado");
+        return pPunto;
+    }
+
+    public PuntoDeAtencionEntity update(PuntoDeAtencionEntity pPunto) 
+    {
+        LOGGER.log(Level.INFO, "Actualizando el punto de atención con id={0}", pPunto.getId());
+        return em.merge(pPunto);
+    }
+
+    public void delete(Long id) 
+    {
+        LOGGER.log(Level.INFO, "Borrando punto de atencióncon id={0}", id);
+        PuntoDeAtencionEntity entity = em.find(PuntoDeAtencionEntity.class, id);
+        em.remove(entity);
+    }
+
+}
